3c056d03a20816a904aca0e36405e09541aa9ad3
Hoa Tran <hoatranhcmiu@gmail.com>
2017-02-26 10:25:31 +0700

    add live link to README.md and default end point to server.js

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # fcc-timestamp-microservice
 Timestamp Microservice: FCC API Projects
+
+Service is available to call:
+
+With format https://hoath-fcc-timestamp.herokuapp.com/[time]
+
+Linux time example:
+
+[https://hoath-fcc-timestamp.herokuapp.com/1450137600](https://hoath-fcc-timestamp.herokuapp.com/1450137600)
+
+Natural time example:
+
+[https://hoath-fcc-timestamp.herokuapp.com/December%2015,%202015](https://hoath-fcc-timestamp.herokuapp.com/December%2015,%202015)
--- server.js
+++ server.js
@@ -1,30 +1,41 @@
-    var express = require('express');
-    var app = express();
-    "use strict";
+var express = require('express');
+var app = express();
+"use strict";
 
-    app.get('/:date', function (req, res){
-        //decode URI
-        var param = decodeURIComponent(req.params.date);
+app.get('/', function (req, res) {
+    var template = `<h1>FCC Timestamp microservice</h1>
+                        <h3>Example usage</h3>
+                        <p>https://hoath-fcc-timestamp.herokuapp.com/December%2015,%202015</p>
+                        <p>https://hoath-fcc-timestamp.herokuapp.com/1450137600</p>
 
-        var unixTime, naturalTime;
-        
-        if(isNaN(param)){
-            unixTime = new Date(param).getTime()/1000;
-            naturalTime = param;
-        }else {
-            console.log(new Date(Number(param)));
-            unixTime = param;
-            naturalTime = new Date(Number(param));
-        }
+                        <h3>Example usage</h3>
+                        { "unix": 1450137600, "natural": "December 15, 2015" }`;
 
-        var result = {
-            unix: unixTime,
-            natural: naturalTime
-        }
-        res.send(JSON.stringify(result));
-    })
+    res.send(template);
+})
+
+app.get('/:date', function (req, res) {
+    //decode URI
+    var param = decodeURIComponent(req.params.date);
+
+    var unixTime, naturalTime;
+
+    if (isNaN(param)) {
+        unixTime = new Date(param).getTime() / 1000;
+        naturalTime = param;
+    } else {
+        unixTime = param;
+        naturalTime = new Date(Number(param));
+    }
+
+    var result = {
+        unix: unixTime,
+        natural: naturalTime
+    }
+    res.send(JSON.stringify(result));
+})
 
 
-    app.listen(process.env.PORT || 3000, function(){
-        console.log('Server is listening on port 3000!');
-    })
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Server is listening on port 3000!');
+})
